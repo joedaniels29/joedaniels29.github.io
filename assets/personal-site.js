94ad6595dc514a73bcf033f35ba9bf7288a94083
Joseph Daniels <joedaniels29@me.com>
2016-08-05 16:06:15 -0400

    sample deploy 5?this should be the last one. Our one liner is now configured

--- assets/personal-site.js
+++ assets/personal-site.js
@@ -3808,7 +3808,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("personal-site/app")["default"].create({"name":"personal-site","version":"0.0.0+fc59fd99"});
+  require("personal-site/app")["default"].create({"name":"personal-site","version":"0.0.0+e8a77965"});
 }
 
 /* jshint ignore:end */
